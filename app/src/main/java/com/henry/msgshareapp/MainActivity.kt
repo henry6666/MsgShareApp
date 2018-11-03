2568a70c9a2a2a5a74186f76cf49d042ab962db0
Henry Aguinaga <aguinagahenry@gmail.com>
2018-11-03 12:40:16 -0500

    Added functionality to Share Data using Implicit Intent to Facebook, Instagram, WhatsApp

--- app/src/main/java/com/henry/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/henry/msgshareapp/MainActivity.kt
@@ -17,12 +17,24 @@ class MainActivity : AppCompatActivity() {
 
             val message: String = et_userMessage.text.toString()
 
-            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+          //  Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 
             val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("user_message", message)
             startActivity(intent)
+        }
 
+        btnShareToOtherApps.setOnClickListener {
 
+            val message: String = et_userMessage.text.toString()
+            val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            intent.putExtra(Intent.EXTRA_TEXT,message)
+
+            intent.type = "text/plain"
+            startActivity(Intent.createChooser(intent, "Share to: "))
         }
     }
+
+
 }
--- app/src/main/java/com/henry/msgshareapp/SecondActivity.kt
+++ app/src/main/java/com/henry/msgshareapp/SecondActivity.kt
@@ -2,6 +2,8 @@ package com.henry.msgshareapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
 
@@ -9,6 +11,15 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
+
+
+        val bundle: Bundle? = intent.extras
+
+        val msg = bundle!!.getString("user_message")
+
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+
+        txvUserMessage.text = msg
 
     }
 }
