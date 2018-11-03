bb8f28170ed4631c84d392791bf45f5ce407c140
Henry Aguinaga <aguinagahenry@gmail.com>
2018-11-03 14:55:20 -0500

    Added Model Class using Data Class of Kotlin

---
+++ app/src/main/java/com/henry/msgshareapp/Model.kt
@@ -0,0 +1,38 @@
+package com.henry.msgshareapp
+
+data class Hobby(var title: String)
+
+object Supplier {
+
+    val hobbies = listOf<Hobby>(
+                    Hobby("Swimming"),
+                    Hobby("Reading"),
+                    Hobby("Walking"),
+                    Hobby("Sleeping"),
+                    Hobby("Gaming"),
+                    Hobby("Programming"),
+                    Hobby("Talking"),
+                    Hobby("Swimming"),
+                    Hobby("Reading"),
+                    Hobby("Walking"),
+                    Hobby("Sleeping"),
+                    Hobby("Gaming"),
+                    Hobby("Programming"),
+                    Hobby("Talking"),
+                    Hobby("Swimming"),
+                    Hobby("Reading"),
+                    Hobby("Walking"),
+                    Hobby("Sleeping"),
+                    Hobby("Gaming"),
+                    Hobby("Programming"),
+                    Hobby("Talking"),
+                    Hobby("Swimming"),
+                    Hobby("Reading"),
+                    Hobby("Walking"),
+                    Hobby("Sleeping"),
+                    Hobby("Gaming"),
+                    Hobby("Programming"),
+                    Hobby("Talking")
+
+    )
+}
