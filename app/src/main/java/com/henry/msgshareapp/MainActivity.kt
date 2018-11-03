13e35691982c11eb82166ca9e0f662a7baa6f063
Henry Aguinaga <aguinagahenry@gmail.com>
2018-11-03 11:05:50 -0500

    Initial commit

---
+++ app/src/main/java/com/henry/msgshareapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.henry.msgshareapp
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        btnSendMessageToNextActivity.setOnClickListener {
+
+            val message: String = et_userMessage.text.toString()
+
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+
+
+        }
+    }
+}
